128cdf3286e33cbc1cca1258954f3ef8fe430873
PastaHedgehog <106383994+PastaHedgehog@users.noreply.github.com>
2022-12-12 21:06:32 +0500

    Add files via upload

---
+++ src/main/java/com/example/a6_1/CunstructSubwayActv.java
@@ -0,0 +1,14 @@
+package com.example.a6_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CunstructSubwayActv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cunstruct_subway_actv);
+    }
+}
